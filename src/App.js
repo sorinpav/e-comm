b69e7a5d66bb5b92d575a4a2f43df81d527bab61
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-03-22 23:24:51 +0000

    routing added on menu items, using react-router-dom and withRouter(). However, the destination pages are yet to be done, will come in subsequent commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
