fa73b5181e3e0f31d1b32da4c45ed667fb553b6a
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-03-23 15:49:59 +0000

    added header component, currently having buttons to go to homepage, contact, shop and signin pages, but no functionality yet. Also, need to add the cart icon, later

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
 import { Switch, Route } from 'react-router-dom';
-
+import Header from './components/header/Header';
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
