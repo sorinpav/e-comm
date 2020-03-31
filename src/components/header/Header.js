973a3121d47ca5f4a0234b7f5eaba1d2aaee1088
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-03-31 19:49:36 +0100

    shopping cart skeleton added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+//Components
+import CartIcon from '../CartIcon/CartIcon';
+import CartDropdown from '../CartDropdown/CartDropdown';
 
 import './Header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -28,8 +31,13 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
-const mapStateToProps = state => ({ currentUser: state.user.currentUser });
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,5 @@
+import { TOGGLE_CART_HIDDEN } from '../types/cartTypes';
+
+export const toggleCartHidden = () => ({
+  type: TOGGLE_CART_HIDDEN
+});
