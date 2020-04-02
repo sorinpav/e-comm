4f92fd7a0f16d12bf0c788cbaf9772d6f4f16196
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-04-02 16:14:57 +0100

    cart dropdown items added (add to cart functionality + UI)

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './CartDropdown.scss';
 import CustomButton from '../CustomButton/CustomButton';
+import { connect } from 'react-redux';
+//components
+import CartItem from '../cartItem/cartItem';
 
-export const CartDropdown = () => (
+export const CartDropdown = ({ items }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {items.map(item => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
-
-export default CartDropdown;
+const mapStateToProps = ({ cart: { items } }) => ({ items });
+export default connect(mapStateToProps, null)(CartDropdown);
