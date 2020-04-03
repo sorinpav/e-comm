e2c974247805267cf5e510abb259abd729b3a84b
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-04-03 19:13:19 +0100

    added shop data to redux and made shopPage.js a functional component

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -4,13 +4,15 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './userReducer';
 import cartReducer from './cartReducer';
 import directoryReducer from './directoryReducer';
+import shopReducer from './shopReducer';
 
 const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
