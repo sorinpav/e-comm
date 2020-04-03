db53b4d3dfa3925b0ad8dfcb5a0491bdaa66f339
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-04-03 18:39:39 +0100

    added directory state to redux and made Directory.js a functional component instead of a class

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userReducer from './userReducer';
 import cartReducer from './cartReducer';
+import directoryReducer from './directoryReducer';
 
 const persistConfig = { key: 'root', storage, whitelist: ['cart'] };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
