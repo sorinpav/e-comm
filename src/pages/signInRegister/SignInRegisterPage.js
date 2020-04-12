3f9051112700b79be2e608fbd42793b086ec8189
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-04-12 22:47:10 +0100

    signInRegister page with styled-components

--- src/pages/signInRegister/SignInRegisterPage.js
+++ src/pages/signInRegister/SignInRegisterPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import './SignInRegisterPage.scss';
 import { SignIn } from '../../components/SignIn/SignIn';
 import { SignUp } from '../../components/SignUp/SignUp';
+import { SignInRegisterContainer } from './SignInRegisterPage.styles';
 function SignInRegisterPage() {
   return (
-    <div className='sign-in-register'>
+    <SignInRegisterContainer>
       <SignIn />
       <SignUp />
-    </div>
+    </SignInRegisterContainer>
   );
 }
 
