48baac4411955a9c132ffd591b3e8d248db334a3
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-04-12 22:44:55 +0100

    collectionPage with styled-components

--- src/pages/collection/CollectionPage.js
+++ src/pages/collection/CollectionPage.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import './CollectionPage.scss';
 import CollectionItem from '../../components/CollectionItem/CollectionItem';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/selectors/shopSelectors';
 
+import {
+  CollectionPageContainer,
+  Title,
+  ItemsContainer,
+} from './CollectionPage.styles';
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   console.log(collection);
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
+    <CollectionPageContainer>
+      <Title>{title}</Title>
+      <ItemsContainer>
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 export default connect(mapStateToProps)(CollectionPage);
